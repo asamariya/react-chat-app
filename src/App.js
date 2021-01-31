ac6b2978593fa358bf9f8dc79f51c74a1881f82a
Abhishek Samariya <asamariya@gmail.com>
2021-01-31 14:07:04 +1100

    Create structure for ChatFeed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-function App() {
-  return <h1>Chat App</h1>;
-}
-
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={process.env.REACT_APP_PROJECT_ID}
+      userName="abhi_master"
+      userSecret={process.env.REACT_APP_USER_SECRET}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
 export default App;
